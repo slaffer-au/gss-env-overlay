ec7bfe0a5978e043c4df31936022e1fc306152da
slaffer.au <slaffer.au@gmail.com>
2015-07-28 19:34:58 +1000

    Prototype complete

--- gss-env-overlay.py
+++ gss-env-overlay.py
@@ -4,18 +4,32 @@ import os
 import subprocess
 import argparse
 
-# Who are the people we have files for?
-gss_users = ["slaffer" , "gchami"]
 
 # Argument parse for username
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description="GSS Environment Overlay.")
 parser.add_argument('--user', '-u', action="store", dest="user", help="Username of GSS user")                    
 args = parser.parse_args()   
 
-def check_user(user):
-    while user not in gss_users:
-        user = input("No environment found for %s.\n\nUsername: ")
+def get_user():
+    
+    # Who are the people we have files for?
+    gss_users = ["slaffer" , "gchami"]    
 
-if not args.user:
-    user = input("Username: ")
-    check_user(user)
+    def check_user(user):
+        while user not in gss_users:
+            user = raw_input("No environment found.\n\nUsername: ")
+        return user
+
+    if not args.user:
+        user = raw_input("Username: ")
+        print("args "+user)
+    
+    else:
+        user = args.user
+    
+    return check_user(user)
+
+
+user = get_user()
+
+subprocess.call(['screen', '-dmS' , user ])
