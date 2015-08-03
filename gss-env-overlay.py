d901ebf310fe9954ce248e85b681913c9c4cf91c
slaffer-au <slaffer.au@gmail.com>
2015-08-03 15:13:24 +0000

    Adding user parsing

--- gss-env-overlay.py
+++ gss-env-overlay.py
@@ -4,12 +4,14 @@ import os
 import subprocess
 import argparse
 import time
-from posix import mkdir
 
 # Argument parse for username
 parser = argparse.ArgumentParser(description="GSS Environment Overlay.")
 parser.add_argument('--user', '-u', action="store", dest="user", help="Username of GSS user")                    
 args = parser.parse_args()   
+
+setup_path = '/etc/gss-env-overlay/'
+support_dir = '/var/support/gss/'
 
 def get_user():  
     if not args.user:
@@ -22,7 +24,8 @@ def get_user():
 
 def check_user(user):      
     # Who are the people we have files for?
-    gss_users = ["slaffer" , "gchami"] 
+    gss_users = os.listdir(setup_path+"/home/")
+    print gss_users
     
     while user not in gss_users:
         user = raw_input("No environment found.\n\nUsername: ")
@@ -34,8 +37,11 @@ def screenrc_setup(user):
     except IOError, e:
 #        if e.errno != 17:
 #            raise 
-        mkdir("/etc/gss-env-overlay", 0755)
+        os.mkdir("/etc/gss-env-overlay", 0755)
         rc_file = open("/etc/gss-env-overlay/screenrc" , "w+")
+    
+    if not os.path.exists(support_dir):
+        os.mkdir(support_dir)
 
     warning = "WARNING: This file will be over-written on each iteration by the script.\n\n"
     rc_file.write(warning)    
@@ -44,7 +50,7 @@ def screenrc_setup(user):
     rc_file.close()
 
 def start_screen(user):
-    bashrc_file = "/tmp/%s.bashrc" % user
+    bashrc_file = "/%s/home/%s/.bashrc" % (setup_path , user)
     bash_setup = "exec /bin/bash -rcfile %s" % bashrc_file
 
     subprocess.call(['screen', '-dmSL' , user , '-c' , "/etc/gss-env-overlay/screenrc" , 'sh' , '-c' , bash_setup])
