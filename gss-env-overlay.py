b7e91db604aefa1a2a930c0a3e2e189d55b4e5af
slaffer.au <slaffer.au@gmail.com>
2015-08-04 15:02:11 +1000

    More work

--- gss-env-overlay.py
+++ gss-env-overlay.py
@@ -11,17 +11,7 @@ parser = argparse.ArgumentParser(description="GSS Environment Overlay.")
 parser.add_argument('--user', '-u', action="store", dest="user", help="Username of GSS user")                    
 args = parser.parse_args()   
 
-        
-def get_user():
-    
-    # Who are the people we have files for?
-    gss_users = ["slaffer" , "gchami"]    
-
-    def check_user(user):
-        while user not in gss_users:
-            user = raw_input("No environment found.\n\nUsername: ")
-        return user
-
+def get_user():  
     if not args.user:
         user = raw_input("Username: ")
     
@@ -30,13 +20,20 @@ def get_user():
     
     return check_user(user)
 
+def check_user(user):      
+    # Who are the people we have files for?
+    gss_users = ["slaffer" , "gchami"] 
+    
+    while user not in gss_users:
+        user = raw_input("No environment found.\n\nUsername: ")
+    return user 
+            
 def screenrc_setup(user):
     try:
         rc_file = open("/etc/gss-env-overlay/screenrc" , "w+")
-    except IOError, ec:
-        if ec.errno != 17:
-            raise 
-        print "Couldn't find that file"
+    except IOError, e:
+#        if e.errno != 17:
+#            raise 
         mkdir("/etc/gss-env-overlay", 0755)
         rc_file = open("/etc/gss-env-overlay/screenrc" , "w+")
 
@@ -47,13 +44,8 @@ def screenrc_setup(user):
     rc_file.close()
 
 def start_screen(user):
-
     bashrc_file = "/tmp/%s.bashrc" % user
-
     bash_setup = "exec /bin/bash -rcfile %s" % bashrc_file
-
-    print bashrc_file
-    print bash_setup
 
     subprocess.call(['screen', '-dmSL' , user , '-c' , "/etc/gss-env-overlay/screenrc" , 'sh' , '-c' , bash_setup])
 
